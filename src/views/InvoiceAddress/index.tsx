4478a46979c6b495a93cda82ba2f9368ba1add6b
vibueno <20514260+vibueno@users.noreply.github.com>
2021-07-12 19:35:26 +0200

    Transform invoice address panel into grid

--- src/views/InvoiceAddress/index.tsx
+++ src/views/InvoiceAddress/index.tsx
@@ -5,35 +5,30 @@ import './index.scss';
 const InvoiceAddress = () => (
   <>
     <div className="invoice-address-panel">
+      <div className="label-group">
+        <label htmlFor="first-name">Vorname</label>,&nbsp;
+        <label htmlFor="last-name">Nachname</label>
+      </div>
       <div className="field-group">
-        <div className="label-group">
-          <label htmlFor="first-name">Vorname</label>,&nbsp;
-          <label htmlFor="last-name">Nachname</label>
-        </div>
-
-        <input id="first-name" type="text" />
-        <input id="last-name" type="text" />
+        <input id="first-name" className="input-field-left" type="text" />
+        <input id="last-name" className="input-field" type="text" />
+      </div>
+      <div className="label-group">
+        <label htmlFor="street">Straße</label>,&nbsp;
+        <label htmlFor="number">Nr.</label>
+      </div>
+      <div className="field-group">
+        <input id="street" className="input-field-left" type="text" />
+        <input id="number" className="input-field" type="text" />
+      </div>
+      <div className="label-group">
+        <label htmlFor="postal-code">PLZ</label>,&nbsp;
+        <label htmlFor="city">Ort</label>
       </div>
 
       <div className="field-group">
-        <div className="label-group">
-          <label htmlFor="street">Straße</label>,&nbsp;
-          <label htmlFor="number">Nr.</label>
-        </div>
-
-        <input id="street" type="text" />
-        <input id="number" type="text" />
-      </div>
-      <div>
-        <div className="field-group">
-          <div className="label-group">
-            <label htmlFor="postal-code">PLZ</label>,&nbsp;
-            <label htmlFor="city">Ort</label>
-          </div>
-
-          <input id="postal-code" type="text" />
-          <input id="city" type="text" />
-        </div>
+        <input id="postal-code" className="input-field-left" type="text" />
+        <input id="city" className="input-field" type="text" />
       </div>
     </div>
   </>
